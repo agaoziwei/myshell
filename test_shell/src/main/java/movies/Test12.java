00247790b3b06c22554f976732081ce3a830a546
agaozisong <491921587@qq.com>
2021-04-03 22:42:13 +0800

    添加了hello word

--- test_shell/src/main/java/movies/Test12.java
+++ test_shell/src/main/java/movies/Test12.java
@@ -3,10 +3,12 @@ public class Test12{
     public static void main(String[] args) {
         System.out.printf("123");
         //进行测试环境1111-------------------
+
     }
     //高子伟添加
     public  void app(){
         System.out.println("app---------------------------");
+        System.out.println("hello word");
     }
 
 }
