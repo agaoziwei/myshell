f2f17b37daadea64778ea8bd6089db87e1862b2e
agaoziwei <agaoziwei@163.com>
2021-04-30 01:49:50 +0800

    mian2222
    (cherry picked from commit 501f3ce94541690fb3dcc033556667ce48e5f93c)

--- test_shell/src/main/java/movies/Test123.java
+++ test_shell/src/main/java/movies/Test123.java
@@ -7,4 +7,8 @@ public class Test123 {
     public static void main(String[] args) {
         System.out.println(1111);
     }
+
+    public static void main2(String[] args) {
+        System.out.println(2222);
+    }
 }
