3444d7521695f5afc71ef8d2e198f59ade642c6a
agaoziwei <agaoziwei@163.com>
2021-04-30 14:11:18 +0800

    mian3333

--- test_shell/src/main/java/movies/Test123.java
+++ test_shell/src/main/java/movies/Test123.java
@@ -3,10 +3,7 @@
  * @description：TODO
  * @date ：2021/4/30 1:31
  */
-public class Test123 {
-    public static void main(String[] args) {
-        System.out.println(1111);
-    }
+
 
     public static void main2(String[] args) {
         System.out.println(2222);
